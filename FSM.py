7fa9c9a9ebbf12a77f98e79788f5c483e66364d3
NeoTh0mas <squanchoffer@gmail.com>
2022-12-06 15:09:32 +0500

    commit

--- FSM.py
+++ FSM.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
 
@@ -27,33 +26,3 @@ class Registration(StatesGroup):
     group = State()
     name = State()
     password = State()
-=======
-from aiogram.dispatcher.filters.state import State, StatesGroup
-
-
-class GeneralStates(StatesGroup):
-    settings = State()
-    select_lang = State()
-
-
-class OrderFood(StatesGroup):
-    init = State()
-    on_menu = State()
-    waiting_for_food = State()
-    confirm = State()
-    continue_or_order = State()
-    order_food = State()
-    payment = State()
-    edit_cart = State()
-
-
-class ReserveTable(StatesGroup):
-    select_table = State()
-
-
-class Registration(StatesGroup):
-    course = State()
-    group = State()
-    name = State()
-    password = State()
->>>>>>> 2b4e0576a6bb5ad7557b0d37e51b800ea9e6a1f8
--- reservation.py
+++ reservation.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from aiogram import Dispatcher
 
 from FSM import ReserveTable, OrderFood
@@ -10,9 +9,9 @@ from db_handlers import profile_find, table_update, table_check, table_find, tab
 # table reservation
 async def reserve(call):
     if call.data == "Забронированные столы":
-        reserved_tables = [x for x in table_find_idle() if x not in [i for i in range(25)]]
+        reserved_tables = [x for x in [i for i in range(1, 25)] if x not in table_find_idle()]
         tables = Inline_kb(reserved_tables)
-        await bot.edit_message_text("Удалите ненужное:", call.from_user.id, call.message.message_id, reply_markup=tables)
+        await bot.edit_message_text(f"Забронированные столы: {', '.join(list(map(str, reserved_tables)))[:-2]}", call.from_user.id, call.message.message_id, reply_markup=tables)
     else:
         profile = profile_find(call.from_user.id)
         if table_check(profile["group"]):
@@ -37,43 +36,3 @@ async def reserve(call):
 # register handlers
 def register_reserve_handlers(dp: Dispatcher):
     dp.register_callback_query_handler(reserve, lambda call: True, state=ReserveTable.select_table)
-=======
-from aiogram import Dispatcher
-
-from FSM import ReserveTable, OrderFood
-from create_bot import bot
-from keyboards import init_keyboard, Inline_kb, Inline
-from db_handlers import profile_find, table_update, table_check, table_find, table_find_idle
-
-
-# table reservation
-async def reserve(call):
-    if call.data == "Забронированные столы":
-        reserved_tables = [x for x in table_find_idle() if x not in [i for i in range(25)]]
-        tables = Inline_kb(reserved_tables)
-        await bot.edit_message_text("Удалите ненужное:", call.from_user.id, call.message.message_id, reply_markup=tables)
-    else:
-        profile = profile_find(call.from_user.id)
-        if table_check(profile["group"]):
-            # some backend to reserve a table (post request to db deleting the table from idle list)
-            await OrderFood.init.set()
-            table_update(int(call.data), profile["name"], profile["surname"], profile["group"])
-            await call.message.answer(f"✅Стол номер {call.data} был успешно забронирован на ваше имя", reply_markup=init_keyboard)
-
-            await bot.send_message(266212760, f"🛎 Стол номер {call.data} был забронирован на имя *{profile['name']}* *{profile['surname']}* и группу *{profile['group']}*!", parse_mode="Markdown")
-
-            await call.answer(f"Вы забронировали стол № {call.data}", show_alert=True)
-            await bot.delete_message(call.from_user.id, call.message.message_id - 1)
-            await bot.delete_message(call.from_user.id, call.message.message_id)
-        else:
-            await call.message.answer("Количество забронированных столов на группу достигло лимита, оставьте места другим)", reply_markup=init_keyboard)
-            await call.answer()
-            await bot.delete_message(call.from_user.id, call.message.message_id - 1)
-            await bot.delete_message(call.from_user.id, call.message.message_id)
-            await OrderFood.init.set()
-
-
-# register handlers
-def register_reserve_handlers(dp: Dispatcher):
-    dp.register_callback_query_handler(reserve, lambda call: True, state=ReserveTable.select_table)
->>>>>>> 2b4e0576a6bb5ad7557b0d37e51b800ea9e6a1f8
